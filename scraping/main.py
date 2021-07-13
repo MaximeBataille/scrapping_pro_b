9b7878feab189ac5a818d16f16ec71c9b997401e
MAXIME BATAILLE <maxime.bataille@sia-partners.com>
2021-07-14 00:58:22 +0200

    match info start

--- scraping/main.py
+++ scraping/main.py
@@ -1,7 +1,7 @@
 # packages
 import classes.scraping as s
 import classes.db_utils as db
-import classes.db_initilize as db_init
+import classes.db_initialize as db_init
 
 if __name__ == '__main__':
 
@@ -23,11 +23,20 @@ if __name__ == '__main__':
     db.truncate_table("TEAMS_STATS_DEF", conn)
     teams_stats_off_df.to_sql("TEAMS_STATS_DEF", engine, if_exists='append', index=False, chunksize=10000)
 
+    # SIMPLIFIER LE SCRAPING pour gagner du temps
     # add new matches into the database
-    # set flag True if played
-    # otherwise keep updating the infos
-    urls_matches = s.get_url_matches()
-    for url_match in urls_matches:
-        s.get_match_info()
+    #db.truncate_table("MATCH", conn) 
+    new_urls_matches = s.new_url_matches()
+    for new_url_match in new_urls_matches:
+        match_info_df = s.get_match_info(new_url_match)
+        match_info_df.to_sql("MATCH", engine, if_exists='append', index=False, chunksize=10000)
+
+    # urls_to_be_updated = s.get_url_match_to_play()
+    # for url_to_be_updated in urls_to_be_updated:
+    #     match_info_df = s.get_match_info(new_url_match)
+    #     ... update with new values
+
+
+    
 
     conn.close()
